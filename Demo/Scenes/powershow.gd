0deb65a380a318144419c017475cecd31eef2ad6
Tizzi <tausifkabir.ca@gmail.com>
2025-09-29 22:02:40 -0400

    working on shop UI

--- Demo/Scenes/powershow.gd
+++ Demo/Scenes/powershow.gd
@@ -5,9 +5,7 @@ signal black_hole_spawned  # 📢 Signal for camera shake
 @onready var anim = $AnimationPlayer
 @onready var animation_player_2: AnimationPlayer = $Sprite2D2/AnimationPlayer2
 @onready var pull_zone = $PullZone
-@onready var idle: AudioStreamPlayer2D = $Idle
-@onready var ka: AudioStreamPlayer = $ka
-@onready var pf: AudioStreamPlayer = $PF
+
 @onready var timer_10s: Timer = $J
 @onready var cooldown_timer: Timer = $CooldownTimer
 @onready var cooldown_bar: ProgressBar = get_node("/root/Game/CanvasLayer3/BHCD")
@@ -20,5 +18,5 @@ var affected_mobs: Array = []
 var affected_player: CharacterBody2D = null
 
 func _ready():
-	idle.play()
+	
 	animation_player_2.play("Black")
